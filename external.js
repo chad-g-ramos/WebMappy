230abdafaef8d31e84b81a395591959e9cabc9f3
bek47 <bek47@txstate.edu>
2023-10-05 20:52:41 -0500

    “Reorganized JavaScript”

---
+++ external.js
@@ -0,0 +1,8 @@
+var map = L.map('map').setView([29.884, -97.9384], 14);
+mapLink =
+    '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+L.tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; ' + mapLink + ' Contributors',
+    maxZoom: 18,
+    }).addTo(map);
