8070e6e1f82354734278e8f10ab0adbae0b75db1
evz12 <evz12@txstate.edu>
2023-11-23 02:50:47 -0600

    sticky

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+window.onscroll = function() {myFunction()};
+var topnav = document.getElementById("myTopnav");
+var sticky = topnav.offsetTop;
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    topnav.classList.add("sticky");
+  } else {
+    topnav.classList.remove("sticky");
+  }
+}
 
   require([
     "esri/Map",
