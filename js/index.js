17b84e1803ee670e86173669117618ce3577ffd6
evz12 <evz12@txstate.edu>
2023-11-27 02:16:05 -0600

    copyright, trying to add popups

--- js/index.js
+++ js/index.js
@@ -1,13 +1,3 @@
-window.onscroll = function() {myFunction()};
-var topnav = document.getElementById("myTopnav");
-var sticky = topnav.offsetTop;
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    topnav.classList.add("sticky");
-  } else {
-    topnav.classList.remove("sticky");
-  }
-}
 
   require([
     "esri/Map",
@@ -26,7 +16,16 @@ function myFunction() {
     map:map,
     zoom:5,
     center: [-98, 30],
-    popupEnabled: false,
+    /*popupEnabled: true,
+      popup: {
+        dockEnabled: true,
+        dockOptions: {
+          // dock popup at bottom-right side of view
+          buttonEnabled: false,
+          breakpoint: false,
+          position: "bottom-right"
+        }
+      } */
   });
 
 
@@ -35,12 +34,48 @@ function myFunction() {
   //layer selector visibility toggle, which is controlled by the layer.visible=false
   //statement below in the second layer.fromPortalItem code block
   //-chad
+/*  const popupDesc = {
+
+    title: "{US_L3Key}",
+    content: [
+      {
+        type:"fields",
+        fieldInfos:[
+          {
+        fieldName: "DESC",
+        label: "Description",
+          }
+        ]
+      }
+    ]
+  };
+//  content: "<b>Description:" + "</b> {Description} "};*/
+  /*var template = {
+    title:"{US_L3NAME}",
+    content:[{
+      type: "fields",
+      fieldInfos:[{
+        fieldName:"Descripton",
+        label:"Description",
+        visible: true,
+      },{
+        fieldName:"OBJECTID",
+        label:"OBJECTID",
+        visible:false,
+
+      }]
+    }]
+  }; */
+
   Layer.fromPortalItem({
     portalItem: {
       // id: "2f73c3d6690e439cacaf8a582e6dcf9c"
       // id: "744540718ad94c64bfc7217c93e00e8b"
-      id: "68e00b62a12f4dd9a259b41d230bc026"
+      id: "68e00b62a12f4dd9a259b41d230bc026",
     },
+  //  outFields:["Description"],
+    //popupTemplate: template
+  //  popupTemplate: popupDesc,
 
   }).then(function(layer){
     map.add(layer);
@@ -50,7 +85,8 @@ function myFunction() {
     portalItem: {
       // id: "2f73c3d6690e439cacaf8a582e6dcf9c"
       // id: "744540718ad94c64bfc7217c93e00e8b"
-      id: "110a082f1bb04e6392a58b000fdf0a9a"
+      id: "110a082f1bb04e6392a58b000fdf0a9a",
+
     },
 
   }).then(function(layer){
@@ -75,3 +111,19 @@ function myFunction() {
   view.ui.add(layerList, "top-right");
 
   });
+
+
+/*  const popupDesc = { title: "{US_L3Key}",
+    content: [
+      {
+        type:"fields",
+        fieldInfos:[
+          {
+        fieldName: "Description",
+        label: "Description",
+          }
+        ]
+      }
+    ]
+  };
+//  content: "<b>Description:" + "</b> {Description} "}; */
