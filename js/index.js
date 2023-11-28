8eaad67ffc11b8993a0a85cb487587d0360417cc
evz12 <evz12@txstate.edu>
2023-11-28 00:26:31 -0600

    Added L3 popups

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-  //loads necessary functions 
+  //loads necessary functions
   require([
     "esri/Map",
     "esri/views/MapView",
-    "esri/layers/Layer",
+    "esri/layers/FeatureLayer",
     "esri/widgets/LayerList",
     "esri/widgets/Search",
     "esri/Graphic",
     "esri/symbols/SimpleMarkerSymbol",
     "esri/geometry/Point",
   //  "dojo/domReady!"
-], (Map, MapView, Layer, LayerList, Search, Graphic, SimpleMarkerSymbol, Point) => {
+], (Map, MapView, FeatureLayer, LayerList, Search, Graphic, SimpleMarkerSymbol, Point) => {
   var map = new Map({
     basemap: "gray-vector",
   //  layers: [layer1,layer2]
@@ -20,7 +20,13 @@
     map:map,
     zoom:5,
     center: [-98, 30],
-    popupEnabled: false,
+    popup: {
+        dockEnabled: true,
+        dockOptions: {
+          buttonEnabled: false,
+          breakpoint: false,
+        }
+      }
   });
 
   //SearchWidget on Main Map
@@ -63,33 +69,83 @@ function addMarker(lat, long) {
       console.log("Geolocation is not supported by this browser.");
   }
 
-  //L3 layer created from a Portal layer item id
-  Layer.fromPortalItem({
-    // autocasts as new PortalItem()
+  const layerL3 = new FeatureLayer({
     portalItem: {
       // id: "2f73c3d6690e439cacaf8a582e6dcf9c"
       // id: "744540718ad94c64bfc7217c93e00e8b"
-      id: "68e00b62a12f4dd9a259b41d230bc026"
+      id: "68e00b62a12f4dd9a259b41d230bc026",
     },
-  }).then(function(layer){
-    //adds layer to map
-    map.add(layer);
-  });
+      outFields:["*"],
+      popupTemplate: {
+        title: "{US_L3NAME}",
+        outFields:["*"],
+        fieldInfos: [
+          {
+            fieldName: "OBJECTID",
+            label: "OBJECTID",
+            visible: false
+          },
+          {
+            fieldName: "US_L3NAME",
+            label: "US_L3NAME",
+            visible: true
+          },
+          {
+            fieldName: "Desc_",
+            label: "Desc",
+            visible: true,
+          },
+          {
+            fieldName: "Unique_",
+            label: "Unique",
+            visible: false,
 
-  //L4 layer created from a Portal layer item id
-  Layer.fromPortalItem({
-    // autocasts as new PortalItem()
-    portalItem: {
-      // id: "2f73c3d6690e439cacaf8a582e6dcf9c"
-      // id: "744540718ad94c64bfc7217c93e00e8b"
-      id: "110a082f1bb04e6392a58b000fdf0a9a"
-    },
-  }).then(function(layer){
-    //adds layer to map
-    map.add(layer);
-    layer.visible=false; //defaults the L4 regions to not visible
-  });
+          },
+          {
+            fieldName: "Shape_Area",
 
+            label: "Area",
+            visible: false,
+            format: {
+                places: 2,
+                  digitSeparator: true
+                }
+          },
+          {
+            fieldName: "Shape_Length",
+
+            label: "Length",
+            visible: false,
+            format: {
+                  places: 2,
+                  digitSeparator: true
+                }
+          },
+
+        ],
+        content: [
+          {
+            type:"text",
+            text: "{Desc_}"
+          },
+          {
+            type:"text",
+            text:"{Unique_}"
+          },
+        ]
+      }
+});
+  map.add(layerL3);
+
+const layerL4 = new FeatureLayer({
+  portalItem: {
+    // id: "2f73c3d6690e439cacaf8a582e6dcf9c"
+    // id: "744540718ad94c64bfc7217c93e00e8b"
+    id: "110a082f1bb04e6392a58b000fdf0a9a",
+  }
+});
+  map.add(layerL4);
+  layerL4.visible=false;
 
   //create an instance of the layer list widget, added to view, closed legend added to panel
   const layerList = new LayerList({
